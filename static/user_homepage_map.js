806b1688faa2784633c1279058a449ceeaa5de14
nataliaiwona <niwonaostrowski@gmail.com>
2016-11-26 07:40:08 +0000

    Added bootstrap to all pages. Need to figure out how to get map to show up agan...

--- static/user_homepage_map.js
+++ static/user_homepage_map.js
@@ -43,7 +43,51 @@ function getThirdOption(pinTypeId) {
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 0, lng: 0},
-        zoom: 2
+        zoom: 2,
+        styles: [
+            {
+                "featureType": "administrative",
+                "elementType": "labels.text.fill",
+                "stylers": [{"color": "#444444"}]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "all",
+                "stylers": [{"color": "#f2f2f2"}]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "all",
+                "stylers": [{"visibility": "off"}]
+            },
+            {
+                "featureType": "road",
+                "elementType": "all",
+                "stylers": [{"saturation": -100},
+                            {"lightness": 45}]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "all",
+                "stylers": [{"visibility": "simplified"}]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels.icon",
+                "stylers": [{"visibility": "off"}]
+            },
+            {   
+                "featureType": "transit",
+                "elementType": "all",
+                "stylers": [{"visibility": "off"}]
+            },
+            {   
+                "featureType": "water",
+                "elementType": "all",
+                "stylers": [{"color": "#46bcec"},
+                            {"visibility": "on"}]
+            }
+        ]
     });
     var infoWindow = new google.maps.InfoWindow({
         width: 350,
